4a272cfbe36fb30c44136e2bfcd4ff0cc4ad1ea8
mahaandika <agungandikabali@gmail.com>
2025-03-31 11:38:07 +0800

    make model

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'name', 'description', 'price', 'image'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
