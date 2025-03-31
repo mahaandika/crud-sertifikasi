2da6e64948c71fd119526b7b8481327aa57cc119
mahaandika <agungandikabali@gmail.com>
2025-03-31 13:58:25 +0800

    fix sidebar

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -13,7 +13,9 @@
 
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item :href="route('dashboard')" :current="request()->routeIs('dashboard')" >{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item :href="route('category')" :current="request()->routeIs('category')" >{{ __('Category') }}</flux:navlist.item>
+                    <flux:navlist.item :href="route('menu')" :current="request()->routeIs('menu')" >{{ __('Menu') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
