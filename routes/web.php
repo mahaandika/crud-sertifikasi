2401bd374b50a5946d60b75caf07a04a29a0f0a4
mahaandika <agungandikabali@gmail.com>
2025-04-02 16:31:37 +0800

    fix routing in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,17 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\WelcomeController;
 use Livewire\Volt\Volt;
 use App\Livewire\MenuList;
 use App\Livewire\CategoryList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 Route::get('/kategoris', CategoryList::class)->name('categories.index');
 Route::get('/menus', MenuList::class)->name('menus.index');
 
